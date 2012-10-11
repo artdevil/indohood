bdbecaa53d86d3a84d15a0edb8a79c1a8cabb88c
artdevil <artdvl91@gmail.com>
2012-10-11 22:11:24 +0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Indohood::Application.config.secret_token = '22baa1db83ec652fe0e0664769c3eb1175f6efc4772ff03f1f694341bf16743c386cb01c7438c95dceac60c5ced3dd6955a17183572d56231c172230195bab13'
